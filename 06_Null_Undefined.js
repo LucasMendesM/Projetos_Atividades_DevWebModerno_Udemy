bb87981604b9d7711acf46fd712060cc0e139e90
LucasMendesM <jpmarquesmendes@gmail.com>
2021-11-29 15:34:36 -0300

    commit 29/11/21

---
+++ 06_Null_Undefined.js
@@ -0,0 +1,5 @@
+// A diferença entre null e undefined é que undefined é um valor padrão, enquanto null é um valor nulo
+
+let valor // Não inicializada (undefined)
+
+valor = null // Ausência de um valor (null)
